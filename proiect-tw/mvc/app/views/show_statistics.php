6ec5be0da5ced6212e5d2d6c15a1ddbfd6f53c5d
TamaraC425 <61472160+TamaraC425@users.noreply.github.com>
2020-06-06 00:43:05 +0300

    change

--- proiect-tw/mvc/app/views/show_statistics.php
+++ proiect-tw/mvc/app/views/show_statistics.php
@@ -5,7 +5,6 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="show_statistics.css">
-        <title>Statistics</title>
     </head>
 <header>
     <link rel="stylesheet" href="index.css">
